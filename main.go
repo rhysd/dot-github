c00dbe11b487b0f3650b7bbe99f80de1dcf79407
rhysd <lin90162@yahoo.co.jp>
2016-02-20 23:13:24 +0900

    get user and name of current repository

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func exitWithUsage() {
@@ -48,6 +51,75 @@ func parseCmdArgs() *Flags {
 	}
 }
 
+func gitCmdPath() string {
+	specified := os.Getenv("DOT_GITHUB_GIT_CMD")
+	if len(specified) != 0 {
+		return specified
+	}
+
+	path, err := exec.LookPath("git")
+	if err != nil {
+		panic("'git' command not found.  Consider to specify $DOT_GITHUB_GIT_CMD manually.")
+	}
+	return path
+}
+
+func originURL() *url.URL {
+	cmd := exec.Command(gitCmdPath(), "ls-remote", "--get-url", "origin")
+	out, err := cmd.Output()
+	if err != nil {
+		panic("'git ls-remote --git-url origin' failed: " + err.Error())
+	}
+	url, err := url.Parse(strings.TrimSpace(string(out[:])))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return url
+}
+
+type Repository struct {
+	user string
+	name string
+}
+
+func NewRepositoryFromHttpsURL(u *url.URL) *Repository {
+	// TODO Check valid GitHub or GHE url
+	if u.Path == "" {
+		panic("Invalid https URL for GitHub: " + u.String())
+	}
+	split := strings.SplitN(u.Path[1:], "/", 2)
+	return &Repository{
+		split[0],
+		strings.TrimSuffix(split[1], ".git"),
+	}
+}
+
+func NewRepositoryFromGitURL(u *url.URL) *Repository {
+	if !strings.HasPrefix(u.Path, "git@") || !strings.Contains(u.Path, ":") {
+		panic("Invalid git@ URL for GitHub: " + u.String())
+	}
+	// TODO Check valid GitHub or GHE url
+	split := strings.SplitN(
+		strings.SplitN(u.Path, ":", 2)[1],
+		"/",
+		2,
+	)
+	return &Repository{
+		split[0],
+		strings.TrimSuffix(split[1], ".git"),
+	}
+}
+
+func NewRepositoryFromURL(u *url.URL) *Repository {
+	if u.Scheme == "https" {
+		return NewRepositoryFromHttpsURL(u)
+	} else if u.Scheme == "" {
+		return NewRepositoryFromGitURL(u)
+	}
+	return nil
+}
+
 func main() {
 	flags := parseCmdArgs()
 	if flags.Help {
@@ -55,4 +127,7 @@ func main() {
 	} else if flags.Version {
 		exitWithVersion()
 	}
+
+	// TODO
+	fmt.Println(NewRepositoryFromURL(originURL()))
 }
