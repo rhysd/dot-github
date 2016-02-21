7aea7550f0f458a3c9f4f9a46a59ff0ffbba7459
rhysd <lin90162@yahoo.co.jp>
2016-02-21 17:34:09 +0900

    describe template file names in usage

--- cli.go
+++ cli.go
@@ -14,11 +14,23 @@ func ExitWithUsage() {
   A CLI tool to generate GitHub files such as CONTRIBUTING.md,
   ISSUE_TEMPLATE.md and PULL_REQUEST_TEMPLATE.md from template files in
   '~/.github' directory.
+
   You can control which template should be used and it attempts to generate
   all by default.
 
+  Below templates are looked by dot-github command.
+  ($DOT_GITHUB_HOME is defaulted to $HOME)
+
+    $DOT_GITHUB_HOME/.github/ISSUE_TEMPLATE.md
+    $DOT_GITHUB_HOME/.github/PULL_REQUEST_TEMPLATE.md
+    $DOT_GITHUB_HOME/.github/CONTRIBUTING.md
+
+
+References:
+
   GitHub Blog: https://github.com/blog/2111-issue-and-pull-request-templates
   More usage:  https://github.com/rhysd/dot-github#readme
+
 
 Flags:`)
 	flag.PrintDefaults()
