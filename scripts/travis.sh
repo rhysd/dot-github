754a5e5f6a2c2922e2831cd0d194ee28777fb255
rhysd <lin90162@yahoo.co.jp>
2017-09-04 20:16:59 +0900

    fix taking a coverage report

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,9 +10,9 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
     go get github.com/axw/gocov/gocov
     go get github.com/mattn/goveralls
-    if ! go get code.google.com/p/go.tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
+    go get golang.org/x/tools/cmd/cover
     go get -t -d -v ./...
-    go test -v -coverprofile=coverage.out ./dotfiles-command/
+    go test -v -coverprofile=coverage.out ./
     $HOME/gopath/bin/goveralls -coverprofile coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
 fi
 
